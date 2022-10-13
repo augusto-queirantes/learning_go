4c7fd625c2f132456d61b2530bd4fa070170cc48
Augusto Queirantes <augusto.queirantes@gmail.com>
2022-10-13 12:25:42 -0300

    Create and configure webserver

---
+++ controllers/messageController.go
@@ -0,0 +1,12 @@
+package controllers
+
+import (
+  "net/http"
+  "github.com/gin-gonic/gin"
+)
+
+func ShowMessage(context *gin.Context) {
+  message := context.Param("message")
+
+  context.JSON(http.StatusOK, gin.H{"message": message})
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+  "github.com/augusto-queirantes/learning_go/config"
+)
+
+func main() {
+  config.Server()
+}
